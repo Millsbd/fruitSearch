4c60fca678d19f3bc3f7d8d782af84f16908ddc1
Millsbd <137799318+Millsbd@users.noreply.github.com>
2023-08-16 09:31:49 -0700

    refactored with review call feedback

--- script copy.js
+++ script copy.js
@@ -1,5 +1,6 @@
 const input = document.querySelector("#fruit");
 const suggestions = document.querySelector(".suggestions ul");
+const sugList = document.querySelector(".list");
 
 const fruit = [
   "Apple",
@@ -85,34 +86,44 @@ const fruit = [
 // // Is there a setting which will let me not have this vertical list happen when I use the prettier formatter?
 
 
-function useSuggestions(array) {
-  suggestions.innerHTML = "";
-  let innerElement = "";
-  for (let itm of array) {
-    innerElement += `<li>${itm}</li>`
+function useSuggestions(array, element) {
+  if (array) {
+    element.innerHTML = "";
+    let innerElement = "";
+    for (let itm of array) {
+      innerElement += `<li>${itm}</li>`
+    }
+    element.innerHTML = innerElement;
+    // console.log(innerElement);
   }
-  suggestions.innerHTML = innerElement;
 }
 // accepts an array and an element, checks the array and resets the html of the element. Loops through the array and for each item adds an li with the item name. Sets the innerHTML of the element equal to this new li. 
 
-function search(array, str) {
-  if(str === ''){
-    return [];
-  }
+function search(array, search) {
   return array.filter((itm) =>
-    itm.toLowerCase().includes(str.toLowerCase())
+    itm.toLowerCase().includes(search.toLowerCase())
   );
 }
 // accepts an array and a string (the search input), filters the array and only returns the values which include the search string. Takes case out of the equation. 
 
-input.addEventListener('input', () => useSuggestions(search(fruit, input.value)));
+input.addEventListener('input', () => {
+  const filterData = search(fruit, input.value);
+  // console.log(filterData)
+  useSuggestions(filterData, sugList);
+});
 
 // whenver something is typed in the searchbox triggers a function which assigns a varialble to the returned array from the filter array function. passes that array into the createItems function, which should make the list of possible searches below the box. 
 
-suggestions.addEventListener('click', function (evt) {
+suggestions.addEventListener('mouseover', function () {
+  suggestions.setAttribute('class', 'ul li:hover');
+})
+// **REFACTOR AS ARROW FUNCTION**
+suggestions.addEventListener('mouseover', () => suggestions.setAttribute('class', 'ul: li:hover'));
+
+suggestions.addEventListener('click', function useSuggestion(evt) {
   let seletctedVal = evt.target.textContent;
   input.value = seletctedVal;
-  suggestions.innerHTML = '';
+
 })
 // ***Trying to get the input value to be the clicked value, shows the li in the console correclty with the console.log but changed the input to 0 not the clicked fruit. Needed to do textContent instead of value, the value was giving the full li where the textContent is giving just the text. 
 
